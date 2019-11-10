cc182bf2167ad5264e88cadbf627a67a3646b305
codyNog <yoneyoneyone99@gmail.com>
2019-11-11 04:01:27 +0900

    selector修正

--- src/components/atoms/Selector.tsx
+++ src/components/atoms/Selector.tsx
@@ -1,36 +1,9 @@
-import React from "react";
-import { css } from "emotion";
+import styled from "@emotion/styled";
 
-interface IPropsSelector {
-  onClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
-  children: React.ReactNode;
-  style?: React.CSSProperties;
-}
-
-const selectorFactory = (role: React.CSSProperties) => (
-  props: IPropsSelector
-) => {
-  const { children, onClick, style } = props;
-  const className = css({ ...role, ...style });
-
-  return (
-    <button className={className} onClick={onClick}>
-      {children}
-    </button>
-  );
-};
-
-const selectorStyle: React.CSSProperties = {
+const Selector = styled("select")({
   height: 30,
   width: 100,
   fontSize: 20
-};
-
-const defaultStyle: React.CSSProperties = {
-  ...selectorStyle,
-  ...{ color: "black" }
-};
-
-const Selector = selectorFactory(defaultStyle);
+});
 
 export default Selector;
