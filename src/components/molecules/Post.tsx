1d9ae28dbd5ea816544dd884eea5d3c1caef11bd
codyNog <yoneyoneyone99@gmail.com>
2019-11-12 18:44:23 +0900

    Postのstyle修正

--- src/components/molecules/Post.tsx
+++ src/components/molecules/Post.tsx
@@ -1,39 +1,44 @@
 import React from "react";
+import styled from "@emotion/styled";
 import { H2 } from "../atoms/Heading";
 import Button from "../atoms/button/Button";
-import { css } from "emotion";
 import { marginM, paddingM, marginS } from "../../constrants/style";
+import { CSSProperties } from "@emotion/serialize";
 
 interface IPropsPost {
   title: string;
   detail: string;
+  style?: CSSProperties;
 }
 
 const Post: React.FC<IPropsPost> = props => {
-  const { title, detail } = props;
+  const { title, detail, style } = props;
 
-  const PostStyle = css({
-    display: "flex",
-    alignItems: "center",
-    border: "1px solid grey",
-    borderRadius: 4,
-    padding: paddingM,
-    marginBottom: marginM
-  });
-
-  const ColumnLeftStyle = css({
+  const ColumnLeft = styled("div")({
     marginRight: marginS,
     flex: 1
   });
 
+  const Wrapper = styled("div")({
+    ...{
+      display: "flex",
+      alignItems: "center",
+      border: "1px solid grey",
+      borderRadius: 4,
+      padding: paddingM,
+      marginBottom: marginM
+    },
+    ...style
+  });
+
   return (
-    <div className={PostStyle}>
-      <div className={ColumnLeftStyle}>
+    <Wrapper>
+      <ColumnLeft>
         <H2>{title}</H2>
         <p>{detail}</p>
-      </div>
+      </ColumnLeft>
       <Button onClick={() => console.log("pushed")}>詳細ページへ</Button>
-    </div>
+    </Wrapper>
   );
 };
 
