01972d7c2998a8d9a891dd13adecbf1ba0ea1052
codyNog <yoneyoneyone99@gmail.com>
2019-11-19 00:57:10 +0900

    routingをリファクタ

--- src/routing/index.tsx
+++ src/routing/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import HomePage from "../components/pages/home/HomePage";
 import { Page } from "../components/layouts";
 import Drawer from "../components/organisms/drawer/Drawer";
 
-const Routing = () => {
+const Routing: React.FC = () => {
   return (
     <BrowserRouter>
       <Page>
